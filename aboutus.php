975fdaddf2c968a3c08971e31977afadeb33b6a7
tameszaza <wasan55411@gmail.com>
2024-07-15 09:40:18 +0700

    Add Thanakrit about us

--- aboutus.php
+++ aboutus.php
@@ -1,20 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <title>About Us</title>
     <link rel="stylesheet" href="styles2.css">
 </head>
+
 <body>
-<header>
+    <header>
         <div class="top-container">
             <ul class="myUL">
-                <li><a href="try2.php"><h5>Home</h5></a></li>
-                <li><a href="contact.php"><h5>Account</h5></a></li>
-                <li><a href="aboutus.php"><h5>About Us</h5></a></li>
-                <li><a href="allerinfo.php"><h5>Learn more</h5></a></li>
+                <li><a href="try2.php">
+                        <h5>Home</h5>
+                    </a></li>
+                <li><a href="contact.php">
+                        <h5>Account</h5>
+                    </a></li>
+                <li><a href="aboutus.php">
+                        <h5>About Us</h5>
+                    </a></li>
+                <li><a href="allerinfo.php">
+                        <h5>Learn more</h5>
+                    </a></li>
             </ul>
         </div>
-</header>
+    </header>
 
     <section class="first" id="about-us">
         <h1>Our Team</h1>
@@ -22,29 +32,41 @@
     <section>
         <div class="column">
             <div class="member">
-            <img src="images/138281.jpg" alt="Image of First member" >
-            <h2>ธนกฤต ดำดวน</h2>
-            <p class="about-text">
-                Welcome to our Food Scanning Programme for Food Allergies. We are dedicated to helping individuals manage their food allergies with ease and confidence. Our innovative scanning technology allows users to quickly identify potential allergens in their meals, ensuring a safer dining experience.
-            </p>
+                <img src="images/138281.jpg" alt="Image of First member">
+                <h2>ธนกฤต ดำดวน</h2>
+                <p class="about-text">
+                    Welcome to our Food Scanning Programme for Food Allergies. We are dedicated to helping individuals
+                    manage their food allergies with ease and confidence. Our innovative scanning technology allows
+                    users to quickly identify potential allergens in their meals, ensuring a safer dining experience.
+                </p>
             </div>
         </div>
         <div class="column">
             <div class="member">
-            <img src="images/138281.jpg" alt="Image of First member" >
-            <h2>ธนกฤต ดำดวน</h2>
-            <p class="about-text">
-                Welcome to our Food Scanning Programme for Food Allergies. We are dedicated to helping individuals manage their food allergies with ease and confidence. Our innovative scanning technology allows users to quickly identify potential allergens in their meals, ensuring a safer dining experience.
-            </p>
+                <img src="images/138281.jpg" alt="Image of First member">
+                <h2>ธนกฤต ดำดวน</h2>
+                <p class="about-text">
+                    <span class="tab-space"></span>"สวัสดีครับผม ธนกฤต ดำดวน สามารถเรียกผมว่าเตมส์ ก็ได้ครับ
+                    ผมเป็นนักเรียน ม.5 โรงเรียนกำเนิดวิทย์ มีความสนใจทางด้านคอมพิวเตอร์ และฟิสิกส์
+                    ในด้านคอมพิวเตอร์ผมมีความสนใจด้าน การพัฒนา machine learning algorithm เป็นพิเศษ
+                    เคยได้ผ่านการอบรมค่าย สอวน.คอมพิวเตอร์ ค่าย 2 ศูนย์สามเสน
+                    เป็นผู้แทนศูนย์โรงเรียนมหิดลวิทยานุสรณ์วิชาฟิสิกส์เข้าร่วมการแข่งขันฟิสิกส์โอลิมปิกระดับชาติได้รับรางวัลเหรียญเงินคับ
+                    นอกจากทางด้านวิชาการแล้วผมยังมีความสนใจทางด้านการถ่ายภาพอีกด้วยครับ"
+                    <br><br>IG: dd_ttnk<br>
+                    Email: Thanakrit.dam64@gmail.com<br>
+                    Tel: 0843292183
+                </p>
             </div>
         </div>
         <div class="column">
             <div class="member">
-            <img src="images/138281.jpg" alt="Image of First member" >
-            <h2>ธนกฤต ดำดวน</h2>
-            <p class="about-text">
-                Welcome to our Food Scanning Programme for Food Allergies. We are dedicated to helping individuals manage their food allergies with ease and confidence. Our innovative scanning technology allows users to quickly identify potential allergens in their meals, ensuring a safer dining experience.
-            </p>
+                <img src="images/138281.jpg" alt="Image of First member">
+                <h2>ธนกฤต ดำดวน</h2>
+                <p class="about-text">
+                    Welcome to our Food Scanning Programme for Food Allergies. We are dedicated to helping individuals
+                    manage their food allergies with ease and confidence. Our innovative scanning technology allows
+                    users to quickly identify potential allergens in their meals, ensuring a safer dining experience.
+                </p>
             </div>
         </div>
     </section>
@@ -56,7 +78,9 @@
         <div class="column" id="advisor-text">
             <h2 id="advisor-name">ธนกฤต ดำดวน</h2>
             <p class="about-text" id="advisor-dess">
-                We believe that everyone deserves to enjoy their meals without the fear of allergic reactions. That’s why we’ve created a user-friendly application that is accessible to everyone. Whether you’re at home, in a restaurant, or traveling, our Food Scanning Programme is designed to be your trusted companion.
+                We believe that everyone deserves to enjoy their meals without the fear of allergic reactions. That’s
+                why we’ve created a user-friendly application that is accessible to everyone. Whether you’re at home, in
+                a restaurant, or traveling, our Food Scanning Programme is designed to be your trusted companion.
             </p>
         </div>
     </section>
@@ -66,10 +90,21 @@
         <p>Email: nscprojectstorage@gmail.com<br>Tel: 0123456789</p>
         <div id="disclaimer">
             <h2>Disclaimer</h2>
-            <p>Agreement 
-This software is a work developed by Adulvitch Kajittanon, Thanakrit Damduan and Phakthada Pitavaratorn from Kamnoetvidya Science Academy (KVIS) under the provision of Dr.Kanes Sumetpipat under Program for food allergy warning in food allergy which has been supported by the National Science and Technology Development Agency (NSTDA), in order to encourage pupils and students to learn and practice their skills in developing software. Therefore, the intellectual property of this software shall belong to the developer and the developer gives  
-NSTDA a permission to distribute this software as an “as is” and non-modified software for a temporary and non-exclusive use without remuneration to anyone for his or her own purpose or academic purpose, which are not commercial purposes. In this connection, NSTDA shall not be responsible to the user for taking care, maintaining, training, or developing the efficiency of this software. Moreover, NSTDA shall not be liable for any error, software efficiency and damages in connection with or arising out of the use of the software.</p>
+            <p>Agreement
+                This software is a work developed by Adulvitch Kajittanon, Thanakrit Damduan and Phakthada Pitavaratorn
+                from Kamnoetvidya Science Academy (KVIS) under the provision of Dr.Kanes Sumetpipat under Program for
+                food allergy warning in food allergy which has been supported by the National Science and Technology
+                Development Agency (NSTDA), in order to encourage pupils and students to learn and practice their skills
+                in developing software. Therefore, the intellectual property of this software shall belong to the
+                developer and the developer gives
+                NSTDA a permission to distribute this software as an “as is” and non-modified software for a temporary
+                and non-exclusive use without remuneration to anyone for his or her own purpose or academic purpose,
+                which are not commercial purposes. In this connection, NSTDA shall not be responsible to the user for
+                taking care, maintaining, training, or developing the efficiency of this software. Moreover, NSTDA shall
+                not be liable for any error, software efficiency and damages in connection with or arising out of the
+                use of the software.</p>
         </div>
     </footer>
 </body>
+
 </html>
