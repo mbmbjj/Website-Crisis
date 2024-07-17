c19b766f15e3d32d8e54af29b6dafe0fd75fb4e2
tameszaza <wasan55411@gmail.com>
2024-07-17 23:05:24 +0700

    I think some think will broke

--- newlogin.php
+++ newlogin.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<title>Food Scanner</title>
+<link rel="stylesheet" href="styles2.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Page</title>
     <script>
         function getCookie(name) {
             const value = `; ${document.cookie}`;
@@ -28,7 +29,32 @@
     </script>
 </head>
 <body>
-    <h1>Login</h1>
+<header>
+        <div class="top-container">
+            <ul class="myUL">
+                <li><a href="try2.php">
+                        <h5>Home</h5>
+                    </a></li>
+                <li><a href="newlogin.php">
+                        <h5>Account</h5>
+                    </a></li>
+                <li><a href="aboutus.php">
+                        <h5>About Us</h5>
+                    </a></li>
+                <li><a href="allerinfo.php">
+                        <h5>Learn more</h5>
+                    </a></li>
+                <li><a href="abt.php">
+                        <h5>Details</h5>
+                    </a></li>
+                <li><a href="file\NSC_26p23e0039_Report_Final01.pdf" download="Allergy_paper.pdf">
+                        <h5>Paper</h5>
+                    </a></li>
+            </ul>
+        </div>
+    </header>
+    <section class="first-section" id="login"><h1 class="Topic">Login</h1></section>
+    
     <form onsubmit="checkLogin(); return false;">
         <label for="username">Username:</label>
         <input type="text" id="username" name="username" required><br><br>
